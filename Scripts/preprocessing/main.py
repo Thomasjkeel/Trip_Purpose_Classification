fcd43f0018b9fdd66ed33f865abe0cc656a1acaf
Thomasjkeel <thomasjames.keel@googlemail.com>
2019-10-20 14:31:36 +0100

    add other shp to main

--- Scripts/preprocessing/main.py
+++ Scripts/preprocessing/main.py
@@ -1,14 +1,16 @@
 import sys
 import geopandas as gpd
 
-def main(filename):
-    print(filename)
+
+def main(filename_16, filename_17):
+    print(filename_16)
     # try:
-        # gdf = gpd.read_file(filename)
+        # gdf_2016 = gpd.read_file(filename)
     # except:
         # return print("Please enter the path to the MTL Trajet Shapefile")
     
 
 if __name__ == '__main__':
-    filename = sys.argv[1]
-    main(filename)
+    filename_16 = sys.argv[1]
+    filename_17 = sys.argv[2]
+    main(filename_16, filename_17)
