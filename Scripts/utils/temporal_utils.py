f6f8d891785fdbb7f9f6abe08b8874945965beb3
Thomasjkeel <thomasjames.keel@googlemail.com>
2019-10-20 16:05:37 +0100

    finish reproj notebook

---
+++ Scripts/utils/temporal_utils.py
@@ -0,0 +1,16 @@
+import pandas as pd
+import datetime
+
+def remove_timezone(data):
+    if not type(data.starttime[0]) == datetime.datetime:
+        data = convert_timestamps_to_datetime(data)
+    data['starttime'] = data['starttime'].apply(lambda dt: dt.replace(tzinfo=None))
+    data['endtime'] = data['starttime'].apply(lambda dt: dt.replace(tzinfo=None))
+    return data
+
+
+def convert_timestamps_to_datetime(data):
+    print("converting start and end timestamps to datetime objects")
+    data['starttime'] = pd.to_datetime(data['starttime'])
+    data['endtime'] = pd.to_datetime(data['endtime'])
+    return data
